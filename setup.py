38458c8e485778db2b856519a8fb753d095b9f11
Harsh kumar gupta <harshseth730@gmail.com>
2026-09-09 08:50:39 +0530

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="harsh kumar",
+    author_email="harshseth730@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
